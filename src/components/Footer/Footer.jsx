40b46c80388143c4f6622c0ad0df965e69e6cb64
eoGabrielo <gabriel144hz@outlook.com>
2024-03-25 14:15:41 -0300

    v2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.containerFooter}>
-        <p>Gabriel Oliveira - 2024</p>
+      <p>Gabriel Oliveira © 2024</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
